00e4dc97c162181c627a5c66263257240c5c2537
danoboss1 <danosehnoutek159@gmail.com>
2026-01-15 10:01:09 +0100

    Fix: wrong colon in tolStatsRouter.ts

--- backend/src/routes/tolStatsRouter.ts
+++ backend/src/routes/tolStatsRouter.ts
@@ -3,7 +3,7 @@ import { TolStatsController } from '../controllers/tolStatsController.js';
 
 const router = express.Router();
 
-router.get(':/statId', TolStatsController.getStat);
+router.get('/:statId', TolStatsController.getStat);
 router.post('/', TolStatsController.saveStat);
 
 export default router;

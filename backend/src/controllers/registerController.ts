1c245b5ceded831361a0a5360a2a2a47b87943f9
danoboss1 <danosehnoutek159@gmail.com>
2026-02-03 19:43:26 +0100

    Feat: Added JWT token into registration process

--- backend/src/controllers/registerController.ts
+++ backend/src/controllers/registerController.ts
@@ -1,6 +1,7 @@
 import type { Request, Response } from "express";
 import bcrypt from 'bcryptjs';
 import userModel from '../models/user.js'
+import jwt from 'jsonwebtoken';
 
 interface RegisterBody {
     username: string;
@@ -14,37 +15,54 @@ interface UserResponse {
 
 export class RegisterController {
     static register = async (req: Request, res: Response) => {
-        let { username, password } = req.body as RegisterBody;
-
-        if (typeof req.body === 'string') {
-            try {
-                const parsedBody = JSON.parse(req.body) as RegisterBody;
-                username = parsedBody.username;
-                password = parsedBody.password;
-            } catch (parseError) {
-                return res.status(400).json({ message: 'Invalid JSON format' });
+        try{
+            let { username, password } = req.body as RegisterBody;
+    
+            if (typeof req.body === 'string') {
+                try {
+                    const parsedBody = JSON.parse(req.body) as RegisterBody;
+                    username = parsedBody.username;
+                    password = parsedBody.password;
+                } catch (parseError) {
+                    return res.status(400).json({ message: 'Invalid JSON format' });
+                }
             }
+    
+            if (!username) {
+                return res.status(400).json({ message: "Username is required" });
+            }
+    
+            if (!password) {
+                return res.status(400).json({ message: "Password is required" });
+            }
+    
+            const existingUser = await userModel.findByUsername(username);
+    
+            if (existingUser) {
+                return res.status(409).json({ message: "Username already exists" });
+            }
+    
+            const userId = await userModel.createUser(username, password);
+    
+            if (!process.env.JWT_SECRET) {
+                throw new Error('JWT secret is not defined');
+            }
+    
+            const token = jwt.sign(
+                { id: userId, username },
+                process.env.JWT_SECRET,
+                { expiresIn: "1h"}
+            );
+            const userResponse: UserResponse = {
+                id: userId,
+                username,
+            };
+    
+            return res.status(201).json({ token, user: userResponse });
+        } catch (err: any) {
+            console.error('Registration error:', err.message);
+
+            res.status(500).json({ message: 'Server error' });
         }
-
-        // existing user tbd
-
-        const userId = await userModel.createUser(username, password);
-
-        // JWT 
-
-        const userResponse: UserResponse = {
-            id: userId,
-            username,
-        }
-
-        // tu este budem vypisavat JWT
-
-        return res.status(201).json({ user: userResponse });
-
-        // a tu errory, ale nemozem zatial lebo if je na JWT 
-
-        // ten endpoint na register funguje a ako tak chapem kodu, potom pokracujem v programovani 
-        // registracie a login-u
-
     };
 }
